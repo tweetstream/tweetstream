d08644274a92bf59edafd0bfb4a65289317ea610
Steve Agalloco <steve.agalloco@gmail.com>
2011-03-31 23:55:23 -0400

    remove require rubygems from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'rubygems'
 require 'tweetstream'
 require 'rspec'
 require 'rspec/autorun'
@@ -21,7 +20,3 @@ def sample_tweets
     @tweets
   end
 end
-
-# Spec::Runner.configure do |config|
-#
-# end
