edeb14b48c2461a1b30972586b8ccf22bb9f1464
Steve Agalloco <steve.agalloco@gmail.com>
2011-04-06 00:23:12 -0400

    accept oauth configuration variables

--- lib/tweetstream/configuration.rb
+++ lib/tweetstream/configuration.rb
@@ -9,7 +9,12 @@ module TweetStream
       :parser,
       :username,
       :password,
-      :user_agent].freeze
+      :user_agent,
+      :auth_method,
+      :consumer_key,
+      :consumer_secret,
+      :oauth_token,
+      :oauth_token_secret].freeze
 
     # The parser that will be used to connect if none is set
     DEFAULT_PARSER = MultiJson.default_engine
@@ -22,6 +27,25 @@ module TweetStream
 
     # The user agent that will be sent to the API endpoint if none is set
     DEFAULT_USER_AGENT = "TweetStream Ruby Gem #{TweetStream::VERSION}".freeze
+
+    # The default authentication method
+    DEFAULT_AUTH_METHOD = :basic
+
+    VALID_FORMATS = [
+      :basic,
+      :oauth].freeze
+
+    # By default, don't set an application key
+    DEFAULT_CONSUMER_KEY = nil
+
+    # By default, don't set an application secret
+    DEFAULT_CONSUMER_SECRET = nil
+
+    # By default, don't set a user oauth token
+    DEFAULT_OAUTH_TOKEN = nil
+
+    # By default, don't set a user oauth secret
+    DEFAULT_OAUTH_TOKEN_SECRET = nil
 
     # @private
     attr_accessor *VALID_OPTIONS_KEYS
@@ -47,6 +71,11 @@ module TweetStream
       self.username           = DEFAULT_USERNAME
       self.password           = DEFAULT_PASSWORD
       self.user_agent         = DEFAULT_USER_AGENT
+      self.auth_method        = DEFAULT_AUTH_METHOD
+      self.consumer_key       = DEFAULT_CONSUMER_KEY
+      self.consumer_secret    = DEFAULT_CONSUMER_SECRET
+      self.oauth_token        = DEFAULT_OAUTH_TOKEN
+      self.oauth_token_secret = DEFAULT_OAUTH_TOKEN_SECRET
       self
     end
   end
