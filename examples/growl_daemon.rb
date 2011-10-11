3f7adc3423e21853094e7442be19334edb02e55f
Steve Agalloco <steve.agalloco@gmail.com>
2011-10-11 13:51:07 -0400

    updated growl example

--- examples/growl_daemon.rb
+++ examples/growl_daemon.rb
@@ -1,12 +1,9 @@
 require 'rubygems'
 require 'tweetstream'
-require 'ruby-growl'
+require 'growl'
 
-if args_start = ARGV.index('--')
-  username, password = ARGV[args_start + 1].split(':')
-  tracks = ARGV[args_start + 2 .. -1]
-  puts "Starting a GrowlTweet to track: #{tracks.inspect}"
-end
+tracks = 'yankees'
+puts "Starting a GrowlTweet to track: #{tracks}"
 
 TweetStream.configure do |config|
   config.consumer_key = 'abcdefghijklmnopqrstuvwxyz'
@@ -16,7 +13,6 @@ TweetStream.configure do |config|
   config.auth_method = :oauth
 end
 
-TweetStream::Daemon.new('tracker').track(*tracks) do |status|
-  g = Growl.new 'localhost', 'growltweet', ['tweet']
-  g.notify 'tweet', status.user.screen_name, status.text
+TweetStream::Daemon.new('tracker').track(tracks) do |status|
+  Growl.notify status.text, :title => status.user.screen_name
 end
