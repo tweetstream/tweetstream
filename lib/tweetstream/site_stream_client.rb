8e56a34f4fa6403bee821d997c4379fcfed41d8e
Steve Agalloco <steve.agalloco@gmail.com>
2012-03-25 22:27:47 -0400

    sitestream client configuration

--- lib/tweetstream/site_stream_client.rb
+++ lib/tweetstream/site_stream_client.rb
@@ -2,5 +2,14 @@ require 'em-http'
 
 module TweetStream
   class SiteStreamClient
+
+    attr_accessor *Configuration::OAUTH_OPTIONS_KEYS
+
+    def initialize(config_uri, oauth = {})
+      options = TweetStream.oauth_options.merge(oauth)
+      Configuration::OAUTH_OPTIONS_KEYS.each do |key|
+        send("#{key}=", options[key])
+      end
+    end
   end
 end
