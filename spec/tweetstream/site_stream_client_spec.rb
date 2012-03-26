2e5d65fb22e05e2e539ef99f20468c749eddc797
Steve Agalloco <steve.agalloco@gmail.com>
2012-03-25 22:27:47 -0400

    sitestream client configuration

--- spec/tweetstream/site_stream_client_spec.rb
+++ spec/tweetstream/site_stream_client_spec.rb
@@ -1,9 +1,64 @@
 require 'spec_helper'
 
 describe TweetStream::SiteStreamClient do
+  before do
+    @keys = TweetStream::Configuration::OAUTH_OPTIONS_KEYS
+  end
 
   describe 'initialization' do
-    pending
+    context 'with module configuration' do
+
+      before do
+        TweetStream.configure do |config|
+          @keys.each do |key|
+            config.send("#{key}=", key)
+          end
+        end
+      end
+
+      after do
+        TweetStream.reset
+      end
+
+      it "should inherit module configuration" do
+        api = TweetStream::SiteStreamClient.new('/config_uri')
+        @keys.each do |key|
+          api.send(key).should == key
+        end
+      end
+    end
+
+    context 'with class configuration' do
+      before do
+        @configuration = {
+          :consumer_key => 'CK',
+          :consumer_secret => 'CS',
+          :oauth_token => 'AT',
+          :oauth_token_secret => 'AS'
+        }
+      end
+
+      context "during initialization" do
+        it "should override module configuration" do
+          api = TweetStream::SiteStreamClient.new('/config_uri', @configuration)
+          @keys.each do |key|
+            api.send(key).should == @configuration[key]
+          end
+        end
+      end
+
+      context "after initilization" do
+        it "should override module configuration after initialization" do
+          api = TweetStream::SiteStreamClient.new('/config_uri')
+          @configuration.each do |key, value|
+            api.send("#{key}=", value)
+          end
+          @keys.each do |key|
+            api.send(key).should == @configuration[key]
+          end
+        end
+      end
+    end
   end
 
   describe '#info' do
