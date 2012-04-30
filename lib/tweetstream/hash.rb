5ddd79c946f0799c6d392a610071bf70395c4134
Erik Michaels-Ober <sferik@gmail.com>
2012-04-30 09:46:58 -0700

    Add shim to make TweetStream::Hash compatible with Ruby < 1.9

--- lib/tweetstream/hash.rb
+++ lib/tweetstream/hash.rb
@@ -7,6 +7,16 @@ class TweetStream::Hash < ::Hash #:nodoc: all
     end
   end
 
+  # This shim is necessary since method_missing won't be invoked for #id on
+  # Ruby < 1.9
+  def id
+    if key?(:id)
+      self[:id]
+    else
+      super
+    end
+  end
+
   def method_missing(method_name, *args)
     if key?(method_name.to_sym)
       self[method_name.to_sym]
