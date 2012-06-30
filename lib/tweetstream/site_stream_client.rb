169a6f8aa3355e6762cf2ec3558a80d8a8252224
Steve Agalloco <steve.agalloco@gmail.com>
2012-06-29 23:36:34 -0400

    dry up the SiteStreamClient

--- lib/tweetstream/site_stream_client.rb
+++ lib/tweetstream/site_stream_client.rb
@@ -28,67 +28,43 @@ module TweetStream
     end
 
     def info(&block)
-      error_msg = 'Failed to retrieve SiteStream info.'
-
-      http = connection.get(:path => info_path)
-      http.callback do
-        if http.response_header.status == 200
-          block.call http.response if block && block.kind_of?(Proc)
-        else
-          @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
-        end
-      end
-      http.errback do
-        @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
-      end
+      options = { :error_msg => 'Failed to retrieve SiteStream info.' }
+      request(:get, info_path, options, &block)
     end
 
     def add_user(user_id, &block)
-      error_msg = 'Failed to add user to SiteStream'
-      user_management(add_user_path, user_id, error_msg, &block)
+      user_id = user_id.join(',') if user_id.kind_of?(Array)
+
+      options = {
+                  :error_msg => 'Failed to add user to SiteStream',
+                  :body => { 'user_id' => user_id }
+                }
+
+      request(:post, add_user_path, options, &block)
     end
 
     def remove_user(user_id, &block)
-      error_msg = 'Failed to remove user from SiteStream.'
-      user_management(remove_user_path, user_id, error_msg, &block)
+      user_id = user_id.join(',') if user_id.kind_of?(Array)
+
+      options = {
+                  :error_msg => 'Failed to remove user from SiteStream.',
+                  :body => { 'user_id' => user_id }
+                }
+
+      request(:post, remove_user_path, options, &block)
     end
 
     def friends_ids(user_id, &block)
-      error_msg = 'Failed to retrieve SiteStream friends ids.'
-
-      http = connection.post(:path => friends_ids_path, :body => { 'user_id' => user_id })
-      http.callback do
-        if http.response_header.status == 200
-          block.call http.response if block && block.kind_of?(Proc)
-        else
-          @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
-        end
-      end
-      http.errback do
-        @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
-      end
+      options = { :error_msg => 'Failed to retrieve SiteStream friends ids.',
+                  :body => { 'user_id' => user_id }
+                }
+      request(:post, friends_ids_path, options, &block)
     end
 
     private
 
-    def user_management(path, user_id, error_msg, &block)
-      user_id = user_id.join(',') if user_id.kind_of?(Array)
-
-      http = connection.post(:path => path, :body => { 'user_id' => user_id })
-      http.callback do
-        if http.response_header.status == 200
-          block.call if block && block.kind_of?(Proc)
-        else
-          @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
-        end
-      end
-      http.errback do
-        @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
-      end
-    end
-
     def connection
-      return @conn if @conn
+      return @conn if defined?(@conn)
 
       @conn = EventMachine::HttpRequest.new('https://sitestream.twitter.com/')
       @conn.use EventMachine::Middleware::OAuth, oauth_configuration
@@ -119,5 +95,27 @@ module TweetStream
     def friends_ids_path
       @config_uri + '/friends/ids.json'
     end
+
+    def request(method, path, options, &block)
+      error_msg = options.delete(:error_msg)
+
+      http = connection.send(method, options.merge(:path => path))
+      http.callback do
+        if http.response_header.status == 200
+          if block && block.kind_of?(Proc)
+            case block.arity
+            when 1 then block.call http.response
+            else block.call
+            end
+          end
+        else
+          @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
+        end
+      end
+      http.errback do
+        @on_error.call(error_msg) if @on_error && @on_error.kind_of?(Proc)
+      end
+    end
+
   end
 end
