5998b767bb001f3dba34099174ca5c8d3bea508e
Erik Michaels-Ober <sferik@gmail.com>
2014-01-17 14:36:14 +0100

    Enforce code coverage minimums

--- spec/helper.rb
+++ spec/helper.rb
@@ -6,7 +6,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   Coveralls::SimpleCov::Formatter
 ]
 SimpleCov.start do
-  add_filter '.bundle'
+  add_filter '/.bundle/'
+  add_filter '/spec/'
+  minimum_coverage(97.87)
 end
 
 require 'tweetstream'
